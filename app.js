f19cbc54f9cc332bb5ec858dd56114791c492a74
ppuritipati-rythmos <ppuritipati@loyaltymethods.com>
2023-10-20 13:06:23 +0530

    netlify updates

--- app.js
+++ app.js
@@ -3,23 +3,22 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express();
-const port = 3333;
 const multer = require('multer');
 
 app.use(bodyParser.json());
 app.use(cors());
 
+const port = process.env.PORT || 3333
+
 const buckets = [];
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        // Specify the directory to save files in
         const bucketName = req.body.bucketName;
         const bucketPath = `./buckets/${bucketName}`;
         cb(null, bucketPath);
     },
     filename: (req, file, cb) => {
-        // Use the original filename for the uploaded file
         cb(null, file.originalname);
     }
 });
@@ -58,7 +57,6 @@ app.post('/create-bucket', (req, res) => {
         return res.status(400).json({ error: 'Bucket directory already exists.' });
     }
 
-    // Create a directory for the new bucket
     const bucketPath = `./buckets/${bucketName}`;
     fs.mkdirSync(bucketPath);
 
@@ -67,7 +65,6 @@ app.post('/create-bucket', (req, res) => {
 });
 
 
-// Add object to the bucket
 app.post('/upload-object', upload.single('file'), (req, res) => {
     const { bucketName } = req.body;
     if (!bucketName) {
@@ -113,13 +110,11 @@ app.delete('/delete-bucket/:bucketName', (req, res) => {
         return res.status(404).json({ error: 'Bucket not found' });
     }
 
-    // Use the 'rimraf' command to delete the bucket directory and its contents
     exec(`rimraf ${bucketPath}`, (error, stdout, stderr) => {
         if (error) {
             return res.status(500).json({ error: 'Failed to delete the bucket' });
         }
 
-        // Remove the bucket from the 'buckets' array or your storage mechanism
         const bucketIndex = buckets.indexOf(bucketName);
         if (bucketIndex !== -1) {
             buckets.splice(bucketIndex, 1);
@@ -157,10 +152,8 @@ app.get('/download-object/:bucketName/:objectKey', (req, res) => {
         return res.status(404).json({ error: 'Object not found' });
     }
 
-    // Set the Content-Disposition header to prompt the browser to download the file
     res.setHeader('Content-Disposition', `attachment; filename="${objectKey}"`);
 
-    // Stream the file to the response
     const fileStream = fs.createReadStream(filePath);
     fileStream.pipe(res);
 });
